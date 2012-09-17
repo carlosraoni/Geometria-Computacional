256cec95f1f853bf9c9d1a50342f013fabff0551
Carlos Raoni Alencar Mendes <carlosraoni@gmail.com>
2012-09-16 21:00:13 -0300

    Melhorando identação.

--- Geometry2D/point2d.cpp
+++ Geometry2D/point2d.cpp
@@ -4,29 +4,31 @@
 
 namespace geometry2d
 {
-	const Point2D ei(1,0);
-		
-	Point2D::Point2D(double x, double y) : m_x(x), m_y(y)
-	{
-		
-	}
 
-	Point2D::~Point2D()
-	{
-		
-	}
-	
-	double Point2D::norm() const{
-		return sqrt(m_x * m_x + m_y * m_y);
-	}
-	
-	double Point2D::angle() const{
-		return geometry2d::angle(ei, *this);
-	}
-	
-	double Point2D::orientedAngle() const{
-		return (m_x < 0) ? -angle(): angle();
-	}
-	
+const Point2D ei(1,0);
+
+Point2D::Point2D(double x, double y) : m_x(x), m_y(y)
+{
+
+}
+
+Point2D::~Point2D()
+{
+
+}
+
+double Point2D::norm() const{
+	return sqrt(m_x * m_x + m_y * m_y);
+}
+
+double Point2D::angle() const{
+	return geometry2d::angle(ei, *this);
+}
+
+double Point2D::orientedAngle() const{
+	return (m_x < 0) ? -angle(): angle();
+}
+
+
 }
 
